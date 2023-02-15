08b3ba5fecb48e6e7bee46fbe3d5df1c438d8e7d
charles6824 <55227491+charles6824@users.noreply.github.com>
2023-02-15 09:09:34 +0100

    bank verify details setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [bankName, setBankName] = useState('')
+  const [accountName, setAccountName] = useState('')
+  const [accountNumber, setAccountNumber] = useState('')
+  function HandleChange (){
+    console.log('click')
+    axios.get(`https://maylancer.org/api/nuban/api.php?account_number=${accountNumber}`)
+    .then(data => {
+      console.log(data)
+      setBankName(data.data.Bank_name)
+      setAccountName(data.data.account_name)
+    })
+  }
+  // function HandleChange (){
+  //   console.log('click')
+  //   fetch(`https://maylancer.org/api/nuban/api.php?account_number=${accountNumber}`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data)
+  //     setBankName(data.Bank_name)
+  //     setAccountName(data.account_name)
+  //   })
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' value={accountNumber} onChange={e => setAccountNumber(e.target.value)} />
+      <button onClick={HandleChange}>Validate</button>
+      <div className=''>
+        <input type='text' value={bankName} readOnly/>
+        <input type='text' value={accountName} readOnly/>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
